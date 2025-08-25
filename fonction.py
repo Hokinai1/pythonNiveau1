30dd4b728b30c2c6840cd5fab53ccb22f8e6e4de
Hokinai1 <hokinaiesso@gmail.com>
2025-08-25 22:07:40 +0000

    Definition d'une fonction en python

---
+++ fonction.py
@@ -0,0 +1,23 @@
+# les fonctions en python
+
+# def nom_fonction( arg1, arg2):
+#     insstruction 1
+#     Instruction 2
+#     return resultat
+
+# definir une fonction qui permet de dire bonjour et bienvenu
+
+# fonction sans arguments sants retour
+def salutation ():
+    print("Bonjour et bienvenue")
+
+
+# fonction sans retour avec arguments
+def hello (nom, prenom):
+    print(f"Bonjour {nom} {prenom}")
+
+# fonction avec argument avec retour
+
+def somme(a, b):
+    c= a + b
+    return c
