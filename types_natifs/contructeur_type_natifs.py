9f96fedc18ccffa610f3352c6aa4845442c4ca2a
Hokinai1 <hokinaiesso@gmail.com>
2025-08-26 00:00:05 +0000

    Les types natifs en python : les string , nombres et les booleens

---
+++ types_natifs/contructeur_type_natifs.py
@@ -0,0 +1,6 @@
+
+
+# str() pour les chaines de cractères
+# int() pour les Nombres Entiers
+# float() pour les Nombres décimaux
+# bool() pour les Booléens
---
+++ types_natifs/type_narifs.py
@@ -0,0 +1,21 @@
+# il y deux types de nommbre 
+
+# les nombre entier 
+nbr = 15
+
+
+
+# les nombre decimaux
+
+nbr_Float = 10.5
+
+
+
+# LES BOOLEENS
+
+
+# les booleen ne peurvent que prendre deux valeur 
+
+# TRUE ou FALSE
+
+bool ("bonjour")
